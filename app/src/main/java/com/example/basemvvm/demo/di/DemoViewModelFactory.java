26fad16dd1d1bfa763b83b609b1a9435e617dd40
hanieh nikjoo <hnk@haniehs-MacBook-Pro.local>
2019-12-06 16:26:39 +0330

    Added DemoViewModelFactory.java

---
+++ app/src/main/java/com/example/basemvvm/demo/di/DemoViewModelFactory.java
@@ -0,0 +1,24 @@
+package com.example.basemvvm.view.demo;
+
+import android.arch.lifecycle.ViewModel;
+import android.support.annotation.NonNull;
+
+import com.example.basemvvm.base.BaseViewModelFactory;
+
+import javax.inject.Inject;
+
+
+public class DemoViewModelFactory extends BaseViewModelFactory {
+
+    @Inject
+    public DemoViewModelFactory(){
+
+    }
+
+    @SuppressWarnings("unchecked")
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(Class<T> modelClass) {
+        return (T) new DemoViewModel(mRestManager,mPrefManager,mDisposables,mSchedulersFacade,mToastLiveData);
+    }
+}

29d83ad908e73d68fcd8aadd2cf85be0c26f1d59
hanieh nikjoo <hnk@haniehs-MacBook-Pro.local>
2019-12-06 16:26:04 +0330

    Added Demo.java

---
+++ app/src/main/java/com/example/basemvvm/demo/di/Demo.java
@@ -0,0 +1,26 @@
+package com.example.basemvvm.view.demo;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sarah on 3/13/2018.
+ */
+
+public class Demo {
+
+    @SerializedName("provincelist")
+    public List<ProvinceModel> provincelist = new ArrayList<>();
+
+    public class ProvinceModel {
+
+        @SerializedName("id")
+        public String id;
+
+        @SerializedName("provincename")
+        public String provincename;
+    }
+
+}
